73c6037e5ebbc0335c7b940f73c0b72792371347
Ammarhr <hariry49@gmail.com>
2020-04-14 03:16:37 +0300

    lab 12

--- server.js
+++ server.js
@@ -13,6 +13,9 @@ const app = express();
 
 const superagent = require('superagent');
 
+const pg = require('pg');
+
+const client = new pg.Client(process.env.DATABASE_URL);
 
 app.set('view engine', 'ejs');
 
@@ -20,9 +23,6 @@ app.use(express.static('./public'));
 
 app.use(cors());
 
-app.get('/', (req, res) => {
-    res.render('./pages/index')
-})
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -30,8 +30,17 @@ app.use(express.urlencoded({ extended: true }));
 app.get('/search', (req, res) => {
     res.render('./pages/searches/new')
 })
-
+app.get('/', (req, res) => {
+    let SQL = 'SELECT * FROM booksData;'
+    client.query(SQL)
+        .then(results => {
+            console.log('asdasdasdasdasdasdas', results.rows);
+            res.render('./pages/index', { bookResults: results.rows });
+        })
+})
+app.post('/book', bookSaver);
 app.post('/searches', bookSearches);
+app.get('/books/:id', getBooksDetails)
 
 function bookSearches(req, res) {
     let newArr = [];
@@ -60,25 +69,55 @@ function bookSearches(req, res) {
                 res.render('./pages/errors', { book: newArr });
             }
         })
-        .catch(error => res.render('pages/errors'));
 }
 
 function Book(data) {
     this.title = data.volumeInfo.title || 'no title avilable';
     this.author = data.volumeInfo.authors || 'no author name avilable';
-    this.summary = data.volumeInfo.description || 'no description avilable';
+    this.summary = data.volumeInfo.description || 'no descriptions avilable';
     if (data.volumeInfo.imageLinks) {
         this.image_url = data.volumeInfo.imageLinks.thumbnail;
     } else {
         this.img = 'https://f0.pngfuel.com/png/981/958/black-and-white-square-illustration-computer-icons-book-symbol-book-now-button-png-clip-art.png';
     }
+
+    if (data.volumeInfo.industryIdentifiers) {
+        this.isbn = `${data.volumeInfo.industryIdentifiers[0].type}  ${data.volumeInfo.industryIdentifiers[0].identifier}`;
+    } else {
+        'no isbn avilable';
+    }
 }
 
+function bookSaver(req, res) {
+    let title = req.body.title;
+    let author = req.body.author;
+    let image_url = req.body.url;
+    let description = req.body.description;
+    let isbn = req.body.isbn;
+    let bookshelf = req.body.bookshelf
+    let SQL = `INSERT INTO booksData (title,author, image_url,description,isbn,bookshelf) VALUES ($1,$2,$3,$4,$5,$6);`
+    let safeValue = [title, author, image_url, description, isbn, bookshelf];
+    return client.query(SQL, safeValue)
+        .then(() => {
+            res.redirect(`/`);
+        })
+}
 
+function getBooksDetails(req, res) {
+    let SQL = 'SELECT * FROM booksData WHERE id=$1;';
+    let values = [req.params.id];
+    return client.query(SQL, values)
+        .then(result => {
+            res.render('pages/books/detail', { book: result.rows[0] });
+        })
+}
 app.use('*', (request, response) => {
     response.render('pages/errors');
 })
 
-app.listen(PORT, () => {
-    console.log(`listening on PORT ${PORT} `)
-})
+client.connect()
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log(`listening on PORT ${PORT} `)
+        })
+    })
